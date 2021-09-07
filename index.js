974e491f7d33caadd5808d6d67b8e529db7136d1
Samuelisch <samuelchanwk@hotmail.com>
2021-09-07 21:35:32 +0800

    creating new Heroku application, restoring 3.9-3.18

--- index.js
+++ index.js
@@ -1,96 +1,103 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
-const Person = require('./models/person')
+const cors = require('cors')
 
-app.use(express.json())
-app.use(express.static('build'))
 app.use(cors())
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-morgan.token('content', function(req, res) { return JSON.stringify(req.body)})
+morgan.token('content', function(request, response) { return JSON.stringify(request.body)})
 
-let persons = []
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
 
 app.get('/info', (request, response) => {
-  const num = persons.length
-  const date = new Date()
-  const htmlContent = `
-    <div>
-      <p>Phonebook has info for ${num} people</p>
-      <p>${date}</p>
-    </div>
-  `
-  response.send(htmlContent)
+  let date  = new Date()
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+  response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(err => next(err))
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-  const id = Number(request.params.id)
-  Person.findByIdAndRemove(id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(err => next(err))
-})
+const generateId = () => {
+  const randId = Math.floor(Math.random() * 1000000)
+  return randId
+}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({ 
-      error: 'input missing' 
+      error: 'name or number missing!' 
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'duplicate name'
     })
   }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
+  const person = {
+    "id": generateId(),
+    "name": body.name,
+    "number": body.number,
+  }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
-// handler of requests with unknown endpoint
+
 app.use(unknownEndpoint)
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
-
-  next(error)
-}
-// this has to be the last loaded middleware.
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3001
+const PORT = process.enc.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
